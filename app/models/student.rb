418d56e5c094a10d4b41abd0e5d5f3fe76471ce4
Ainor Syahrizal <macintorch@gmail.com>
2015-12-18 16:29:07 +0800

    pass name age test

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,23 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+#attr_accessor :name :first_name :last_name
+ validates :uniqueness => true, :format => { :with => /\w+@\w+.[a-z]{3,}.[a-z]{2,}/,
+     :message => "Only email allowed" }
+
+     validates :email, :uniqueness
+	def name
+		"#{first_name} #{last_name}"
+	end
+
+	def age
+
+		now = Date.today.year
+		age = now - self.birthday.year
+	end
 end
+
+
+
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,17 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    	create_table :students do |n|
+    	n.string :first_name
+    	n.string :last_name
+    	n.string :gender
+    	n.date :birthday
+    	n.string :email
+    	n.string :phone
+    	n.timestamps null: false
+    end
+
+
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
